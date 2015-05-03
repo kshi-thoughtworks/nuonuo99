34f7b78934ca7f3c34d814d68ae6bbff31f27b43
Zhu Tao <zhutao.iscas@gmail.com>
2015-05-03 22:44:44 +0800

    unicdoe

--- image/adminx.py
+++ image/adminx.py
@@ -1,3 +1,4 @@
+#coding: utf-8
 from common.base_admin import BaseAdmin
 import xadmin
 from .models import Image
@@ -11,9 +12,10 @@ class ImageAdmin(BaseAdmin):
         else:
             body += "<span>No Image Uploaded.</span>"
         return body
-    show_image.short_description = "头像"
+    show_image.short_description = "图片"
     show_image.allow_tags = True
     show_image.is_column = True
+
     list_display = ("member", "show_image", "content_type", "object_id")
 
 
