c88eb3c692c9889a1e96483aa58b7a8f04dc22c2
shikai <shikai590@163.com>
2015-05-03 16:38:49 +0800

    加入模型汉化

--- image/models.py
+++ image/models.py
@@ -7,11 +7,11 @@ from django.contrib.contenttypes.models import ContentType
 class Image(BaseModel):
     """image for product, etc.
     """
-    member = models.ForeignKey("member.Member", related_name="uploaded_images")
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
+    member = models.ForeignKey("member.Member", related_name="uploaded_images",verbose_name='上传会员名')
+    content_type = models.ForeignKey(ContentType,verbose_name='图片对应对象类别')
+    object_id = models.PositiveIntegerField(verbose_name='图片对应对象ID')
     content_object = GenericForeignKey('content_type', 'object_id')
-    image = models.ImageField(upload_to="static/images/%Y/%m/%d", null=True)
+    image = models.ImageField(upload_to="static/images/%Y/%m/%d", null=True,verbose_name='对应图片')
 
     class Meta:
         verbose_name = "图片"
