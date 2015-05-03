98b03d6063b67168425eee01daef7da997754e0c
Zhu Tao <zhutao.iscas@gmail.com>
2015-05-03 22:43:29 +0800

    fix an issue

--- image/adminx.py
+++ image/adminx.py
@@ -11,6 +11,9 @@ class ImageAdmin(BaseAdmin):
         else:
             body += "<span>No Image Uploaded.</span>"
         return body
+    show_image.short_description = "头像"
+    show_image.allow_tags = True
+    show_image.is_column = True
     list_display = ("member", "show_image", "content_type", "object_id")
 
 
