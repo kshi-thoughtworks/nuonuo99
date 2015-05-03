dfe0e54a3fdcfcb1d12686d3dd47a6c6b91d6e21
Zhu Tao <zhutao.iscas@gmail.com>
2015-05-03 22:06:40 +0800

    show images for member

--- member/adminx.py
+++ member/adminx.py
@@ -1,9 +1,23 @@
+#coding: utf-8
 from common.base_admin import BaseAdmin
 import xadmin
 from .models import Member, Payment, OAuthInfo
 
+
 class MemberAdmin(BaseAdmin):
-    list_display = ("user", "gender", "city", "region", "location")
+    def member_thumbnail(self, memberadmin):
+        body = ""
+        if memberadmin.icon:
+            body += '<img src="%s" style="width:80px;" title="%s"/>' % (
+                memberadmin.icon.url, memberadmin.user.username)
+        else:
+            body += "<span>No Image Uploaded.</span>"
+        return body
+    member_thumbnail.short_description = "头像"
+    member_thumbnail.allow_tags = True
+    member_thumbnail.is_column = True
+
+    list_display = ("user", "member_thumbnail", "gender", "city", "region", "location")
     list_filters = ("gender", "type", "nn_created_at", "nn_status")
 
 class PaymentAdmin(BaseAdmin):
