570e1fae91196d6d927b52014a9b3259f671ef67
shikai <shikai590@163.com>
2015-05-04 18:07:59 +0800

    加入模型汉化

--- service/models.py
+++ service/models.py
@@ -5,27 +5,27 @@ from common.choices import HotelStyleChoices, HostStyleChoices
 
 
 class Service(BaseModel):
-    name = models.CharField(max_length=255, db_index=True)
-    desc = models.TextField(blank=True)
-    provider = models.ForeignKey("provider.Provider")
-    executor = models.ForeignKey("provider.Executor")
-    price = models.FloatField(default=0)
-    fans_cnt = models.IntegerField(default=0)
-    like_cnt = models.IntegerField(default=0)
-    comment_cnt = models.IntegerField(default=0)
-    fav_cnt = models.IntegerField(default=0)
+    name = models.CharField(max_length=255, db_index=True,verbose_name='服务名称')
+    desc = models.TextField(blank=True,verbose_name='服务描述')
+    provider = models.ForeignKey("provider.Provider",verbose_name='供应商')
+    executor = models.ForeignKey("provider.Executor",verbose_name='执行人')
+    price = models.FloatField(default=0,verbose_name='服务价格')
+    fans_cnt = models.IntegerField(default=0,verbose_name='粉丝数量')
+    like_cnt = models.IntegerField(default=0,verbose_name='赞数量')
+    comment_cnt = models.IntegerField(default=0,verbose_name='评论数量')
+    fav_cnt = models.IntegerField(default=0,verbose_name='关注数量')
 
     class Meta:
         abstract = True
 
 class HotelService(Service):
     """酒店服务"""
-    style = models.IntegerField(choices=HotelStyleChoices.CHOICES, default=HotelStyleChoices.DEFAULT)
-    city = models.IntegerField(null=True)
-    region = models.IntegerField(null=True)
-    location = models.IntegerField(null=True)
-    address = models.TextField(blank=True)
-    hall = models.IntegerField(null=True)
+    style = models.IntegerField(choices=HotelStyleChoices.CHOICES, default=HotelStyleChoices.DEFAULT,verbose_name='酒店类型')
+    city = models.IntegerField(null=True,verbose_name='市')
+    region = models.IntegerField(null=True,verbose_name='区域')
+    location = models.IntegerField(null=True,verbose_name='位置')
+    address = models.TextField(blank=True,verbose_name='地址信息')
+    hall = models.IntegerField(null=True,verbose_name='宴会厅数量')
 
 
     class Meta:
@@ -80,20 +80,20 @@ class AutoService(Service):
     pass
 
     class Meta:
-        verbose_name = "车队服务"
+        verbose_name = "婚车服务"
         verbose_name_plural = verbose_name
 
 class HallService(Service):
     """宴会厅服务"""
-    tall = models.FloatField(default=0)
-    size = models.FloatField(default=0)
-    address = models.TextField(blank=True)
-    best_table_cnt = models.IntegerField(default=0)
-    mini_table_cnt = models.IntegerField(default=0)
-    max_table_cnt = models.IntegerField(default=0)
-    has_led = models.BooleanField(default=True)
-    has_sound_equipment = models.BooleanField(default=True)
-    wedding_cnt = models.IntegerField(default=0)
+    tall = models.FloatField(default=0,verbose_name='高度')
+    size = models.FloatField(default=0,verbose_name='面积')
+    address = models.TextField(blank=True,verbose_name='地址')
+    best_table_cnt = models.IntegerField(default=0,verbose_name='最佳容纳桌数')
+    mini_table_cnt = models.IntegerField(default=0,verbose_name='最少容纳桌数')
+    max_table_cnt = models.IntegerField(default=0,verbose_name='最大容纳桌数')
+    has_led = models.BooleanField(default=True,verbose_name='是否有LED屏')
+    has_sound_equipment = models.BooleanField(default=True,verbose_name='是否有音响设备')
+    wedding_cnt = models.IntegerField(default=0,verbose_name='办过婚礼场次')
 
     class Meta:
         verbose_name = "宴会厅服务"
